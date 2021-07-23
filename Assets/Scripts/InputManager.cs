75b520c4fbf1ef886da9293018f72e5b70f3349b
Andy SHU <andy091045@gmail.com>
2021-07-23 22:48:47 +0800

    宣告變數

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager Instance; // 設定這邊的數值為全域變數
+    public int planes; //一個玩家要幾個地板
+    public float planespeed; //地板沉下去的速度
+    public float playerSpeed; //玩家移動速度 
+
+}

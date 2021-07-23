0d653b886ac75fbfdcb26dedcc3abbe4ec8432f9
S901059999 <s901052002@yahoo.com.tw>
2021-07-23 23:42:15 +0900

    Add common scripts & shapes.

---
+++ Assets/Scripts/Common/ObjectExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public static class ObjectExtensionMethods {
+
+    public static T Instantiate<T>(Object original, Vector3 position) where T : Object {
+        return Object.Instantiate(original as T, position, Quaternion.identity);
+    }
+
+    public static T Instantiate<T>(Object original, Vector3 position, Transform parent) where T : Object {
+        return Object.Instantiate(original as T, position, Quaternion.identity, parent);
+    }
+
+    /// <summary>
+    /// Set position from parent's position.
+    /// </summary>
+    public static T Instantiate<T>(Object original, Transform parent, Vector3 position) where T : Object {
+        return Object.Instantiate(original as T, parent.position + position, Quaternion.identity, parent);
+    }
+}

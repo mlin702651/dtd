719b211bc74a3d4989dd0df697ec39881e7c9b1e
Andy SHU <andy091045@gmail.com>
2021-07-24 11:35:20 +0800

    ground

--- Assets/Scripts/GroundGenerator.cs
+++ Assets/Scripts/GroundGenerator.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GroundGenerator : MonoBehaviour
 {
+    public Transform ground; //地板的總物件
     public GameObject inputman;
     public GameObject dirtblock;
     public GameObject groundctrl;
@@ -14,27 +15,30 @@ public class GroundGenerator : MonoBehaviour
     private float planespeed; //地板沉下去的速度
     void Start()
     {
-        width=Screen.width/200;
-        height=Screen.height/200;
-        planes=ParameterManager.Instance.planes;
-        planespeed=ParameterManager.Instance.planespeed;
+        width = Screen.width / 200;
+        height = Screen.height / 200;
+        planes = ParameterManager.Instance.planes;
+        planespeed = ParameterManager.Instance.planespeed;
         Genmap();
-        for(int i=0;i<ParameterManager.Instance.startblockamount;i++){
+        for (int i = 0; i < ParameterManager.Instance.startblockamount; i++)
+        {
             groundctrl.GetComponent<GroundController>().rise(-1f);
             groundctrl.GetComponent<GroundController>().rise(1f);
         }
     }
-    public void Genmap(){
-        for(int i=-planes;i<planes;i++){
-            Vector3 pos=defaultt.position;
-            pos.x+=i<0?(width/planes)*(i+0.5f):(width/planes)*(i+0.5f);
-            pos.y=-height*2;
-            pos.z-=0.1f;
-            GameObject dirt=Instantiate(dirtblock,pos,Quaternion.identity);
-            dirt.transform.SetLocalScaleX(width/planes);
+    public void Genmap()
+    {
+        for (int i = -planes; i < planes; i++)
+        {
+            Vector3 pos = defaultt.position;
+            pos.x += i < 0 ? (width / planes) * (i + 0.5f) : (width / planes) * (i + 0.5f);
+            pos.y = -height * 2;
+            pos.z -= 0.1f;
+            GameObject dirt = Instantiate(dirtblock, pos, Quaternion.identity, ground);
+            dirt.transform.SetLocalScaleX(width / planes);
             //dirt.transform.SetLocalScaleY(height);
             //dirt.GetComponent<BoxCollider2D>().size=new Vector2(2f,2f);
-            dirt.name="土塊"+i;
+            dirt.name = "土塊" + i;
             dirt.SetActive(true);
             ParameterManager.Instance.dirtlist.Add(dirt);
         }

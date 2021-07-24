effa0cff11d2308eae06361e5cec41973fa44bed
Andy SHU <andy091045@gmail.com>
2021-07-24 21:10:00 +0800

    CorrectJump

--- Assets/Scripts/GroundGenerator.cs
+++ Assets/Scripts/GroundGenerator.cs
@@ -27,19 +27,21 @@ public class GroundGenerator : MonoBehaviour
             groundctrl.GetComponent<GroundController>().rise(1f);
         }
     }
-    public void Genmap(){
-        for(int i=-planes;i<planes;i++){
-            Vector3 pos=defaultt.position;
-            pos.x+=i<0?(width/planes)*(i+0.5f):(width/planes)*(i+0.5f);
-            pos.y=-height*2;
-            pos.z-=0.1f;
-            GameObject dirt=Instantiate(dirtblock,pos,Quaternion.identity,ground);
-            dirt.transform.SetLocalScaleX(width/planes/1.15f);
+    public void Genmap()
+    {
+        for (int i = -planes; i < planes; i++)
+        {
+            Vector3 pos = defaultt.position;
+            pos.x += i < 0 ? (width / planes) * (i + 0.5f) : (width / planes) * (i + 0.5f);
+            pos.y = -height * 2;
+            pos.z -= 0.1f;
+            GameObject dirt = Instantiate(dirtblock, pos, Quaternion.identity, ground);
+            dirt.transform.SetLocalScaleX(width / planes / 1.15f);
             //dirt.transform.SetLocalScaleY(height);
             //dirt.GetComponent<BoxCollider2D>().size=new Vector2(2f,2f);
-            dirt.name="土塊"+i;
-            SpriteRenderer dirtsp=dirt.GetComponent<SpriteRenderer>();
-            dirtsp.sprite=blocks[(i+planes)%3];
+            dirt.name = "土塊" + i;
+            SpriteRenderer dirtsp = dirt.GetComponent<SpriteRenderer>();
+            dirtsp.sprite = blocks[(i + planes) % 3];
             dirt.SetActive(true);
             ParameterManager.Instance.dirtlist.Add(dirt);
         }
@@ -47,7 +49,8 @@ public class GroundGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Random.Range(0f,1f)<=ParameterManager.Instance.spawnchance){
+        if (Random.Range(0f, 1f) <= ParameterManager.Instance.spawnchance)
+        {
             groundctrl.GetComponent<GroundController>().rise(-1f);
             groundctrl.GetComponent<GroundController>().rise(1f);
         }

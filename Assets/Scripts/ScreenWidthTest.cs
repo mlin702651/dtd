878cc221c6445b47c828220bb5eb7f1cd50a7268
Andy SHU <andy091045@gmail.com>
2021-07-24 10:21:19 +0800

    HairDryerPush1021

---
+++ Assets/Scripts/ScreenWidthTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenWidthTest : MonoBehaviour
+{
+    void Start()
+    {
+        //Output the current screen window width in the console
+        Debug.Log("Screen Width : " + Screen.width);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

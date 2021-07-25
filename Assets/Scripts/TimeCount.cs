b7a7b7dedffbb83a981e89e13ec105fcafd1f786
Andy SHU <andy091045@gmail.com>
2021-07-25 14:00:57 +0800

    CROWN

--- Assets/Scripts/TimeCount.cs
+++ Assets/Scripts/TimeCount.cs
@@ -5,19 +5,21 @@ using UnityEngine.UI;
 
 public class TimeCount : MonoBehaviour
 {
+    public GameObject timeUp;
     public GameObject UNIT;
     public GameObject TEN;
 
+    public GameObject LCrown;
+
+    public GameObject RCrown;
     SpriteRenderer unitSprite;
     SpriteRenderer tenSprite;
     public Sprite[] numberSprite;
     public GameObject RButton;
     public GameObject EButton;
     float timeCount;
-    public Text timeCount_UI;
     public GameObject END_UI;
     public GameObject Win_UI;
-    public Text win_UI;
     int addd; //遊戲開始倒數
 
     int unitPlace = 0;
@@ -29,7 +31,6 @@ public class TimeCount : MonoBehaviour
         timeCount = ParameterManager.Instance.TimeCount;
         unitSprite = UNIT.GetComponent<SpriteRenderer>();
         tenSprite = TEN.GetComponent<SpriteRenderer>();
-        // END_UI.SetActive(false);
         InvokeRepeating("timer", 1, 1);
     }
 
@@ -37,9 +38,14 @@ public class TimeCount : MonoBehaviour
     {
         if (addd == 0)
         {
-            timeCount--;
-            unitPlace = (int)timeCount / 1 % 10;
-            tenPlace = (int)timeCount / 10 % 10;
+
+            if (timeCount > 0)
+            {
+                unitPlace = (int)timeCount / 1 % 10;
+                tenPlace = (int)timeCount / 10 % 10;
+                timeCount--;
+            }
+
         }
         else
         {
@@ -47,25 +53,24 @@ public class TimeCount : MonoBehaviour
         }
         unitSprite.sprite = numberSprite[unitPlace];
         tenSprite.sprite = numberSprite[tenPlace];
-        // timeCount_UI.text = timeCount + "";
 
         if (timeCount == 0)
         {
-            timeCount_UI.text = "0";
-            END_UI.SetActive(true);
-            Win_UI.SetActive(true);
+            UNIT.SetActive(false);
+            TEN.SetActive(false);
+            timeUp.SetActive(true);
             RButton.SetActive(true);
             EButton.SetActive(true);
             switch (ParameterManager.Instance.win_status)
             {
                 case -1:
-                    win_UI.text = "LeftWin!";
+                    LCrown.SetActive(true);
                     break;
                 case 0:
-                    win_UI.text = "Tie!";
+
                     break;
                 case 1:
-                    win_UI.text = "RightWin!";
+                    RCrown.SetActive(true);
                     break;
                 default:
                     Debug.Log("Default case");

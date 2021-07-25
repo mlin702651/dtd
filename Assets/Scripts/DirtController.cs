1b01f3dcfdd9443441bffa533f28d439dfabd8e4
Andy SHU <andy091045@gmail.com>
2021-07-25 17:16:49 +0800

    finalExceptArt

--- Assets/Scripts/DirtController.cs
+++ Assets/Scripts/DirtController.cs
@@ -7,8 +7,8 @@ public class DirtController : MonoBehaviour
     // Start is called before the first frame update
     public string dirtstate;
     public int risestack = 0;
-    public bool isspeacial=false;
-    public bool isdangerous=false;
+    public bool isspeacial = false;
+    public bool isdangerous = false;
     public float danger;
     private float temptime;
     private float colortime;
@@ -18,9 +18,9 @@ public class DirtController : MonoBehaviour
     private bool coloring;
     void Start()
     {
-        blocolor=ParameterManager.Instance.blockcolor;
-        dangerouscolor=ParameterManager.Instance.dangerouscolor;
-        specialcolor=ParameterManager.Instance.specialcolor;
+        blocolor = ParameterManager.Instance.blockcolor;
+        dangerouscolor = ParameterManager.Instance.dangerouscolor;
+        specialcolor = ParameterManager.Instance.specialcolor;
         temptime = -1;
         transform.SetPositioinY(transform.position.y + ParameterManager.Instance.planespeed * risestack);
         risestack = 0;
@@ -41,13 +41,18 @@ public class DirtController : MonoBehaviour
                 {
                     temptime++;
                     transform.SetPositioinY(transform.position.y + ParameterManager.Instance.planespeed / ParameterManager.Instance.groundchangingTime * temptime);
+                    if (isdangerous)
+                    {
+                        transform.SetPositioinY(transform.position.y + ParameterManager.Instance.planespeed / ParameterManager.Instance.groundchangingTime * temptime / 2);
+                    }
                 }
                 else
                 {
                     if (risestack == 0)
                     {
-                        if(Random.Range(0f,1f)<=ParameterManager.Instance.specialspawn){
-                            isspeacial=true;
+                        if (Random.Range(0f, 1f) <= ParameterManager.Instance.specialspawn)
+                        {
+                            isspeacial = true;
                         }
                         transform.GetChild(0).gameObject.SetActive(true);
                         dirtstate = "none";
@@ -65,9 +70,10 @@ public class DirtController : MonoBehaviour
                 {
                     temptime = 0;
                     transform.GetChild(0).gameObject.SetActive(false);
-                    if(isspeacial){
-                        isspeacial=false;
-                        this.GetComponent<SpriteRenderer>().color=ParameterManager.Instance.blockcolor;
+                    if (isspeacial)
+                    {
+                        isspeacial = false;
+                        this.GetComponent<SpriteRenderer>().color = ParameterManager.Instance.blockcolor;
                         //do something special
                     }
                 }
@@ -79,7 +85,7 @@ public class DirtController : MonoBehaviour
                 else
                 {
                     transform.GetChild(0).gameObject.SetActive(true);
-                    if (transform.position.y <= -ParameterManager.Instance.height*2)
+                    if (transform.position.y <= -ParameterManager.Instance.height * 2)
                     {
                         transform.GetChild(0).gameObject.SetActive(false);
                     }
@@ -90,33 +96,41 @@ public class DirtController : MonoBehaviour
             default:
                 break;
         }
-        if(isspeacial||isdangerous){
-            if(isdangerous){
-                blocolor=Color.Lerp(ParameterManager.Instance.blockcolor,dangerouscolor,ParameterManager.Instance.fadetime*colortime);
+        if (isspeacial || isdangerous)
+        {
+            if (isdangerous)
+            {
+                blocolor = Color.Lerp(ParameterManager.Instance.blockcolor, dangerouscolor, ParameterManager.Instance.fadetime * colortime);
             }
-            else{
-                blocolor=Color.Lerp(ParameterManager.Instance.blockcolor,specialcolor,ParameterManager.Instance.fadetime*colortime);
+            else
+            {
+                blocolor = Color.Lerp(ParameterManager.Instance.blockcolor, specialcolor, ParameterManager.Instance.fadetime * colortime);
             }
-            if(coloring){
-                    colortime+=Time.deltaTime;
-                }
-                else{
-                    colortime-=Time.deltaTime;
-                }
-                if(colortime>ParameterManager.Instance.fadetime){
-                    coloring=false;
-                    colortime=ParameterManager.Instance.fadetime;
-                }
-                else if(colortime<0){
-                    coloring=true;
-                    colortime=0;
-                }
-            danger-=Time.deltaTime;
-            if(danger<=0){
-                isdangerous=false;
-                blocolor=ParameterManager.Instance.blockcolor;
+            if (coloring)
+            {
+                colortime += Time.deltaTime;
             }
-            this.GetComponent<SpriteRenderer>().color=blocolor;
+            else
+            {
+                colortime -= Time.deltaTime;
+            }
+            if (colortime > ParameterManager.Instance.fadetime)
+            {
+                coloring = false;
+                colortime = ParameterManager.Instance.fadetime;
+            }
+            else if (colortime < 0)
+            {
+                coloring = true;
+                colortime = 0;
+            }
+            danger -= Time.deltaTime;
+            if (danger <= 0)
+            {
+                isdangerous = false;
+                blocolor = ParameterManager.Instance.blockcolor;
+            }
+            this.GetComponent<SpriteRenderer>().color = blocolor;
         }
     }
 }
